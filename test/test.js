ffa3135fda667afd06e14e0daa75c72553e42b8c
rdecos <rdecos@gmail.com>
2023-04-27 11:33:10 +0200

    first commit

---
+++ test/test.js
@@ -0,0 +1,12 @@
+//const { expect } = require("chai")
+
+//const { describe } = require('node:test');
+
+var expect = require('chai').expect;
+
+describe('set de pruebas 1',function(){
+    it ('test1',function(){
+
+        expect(true).to.equal(true);
+    });
+});
